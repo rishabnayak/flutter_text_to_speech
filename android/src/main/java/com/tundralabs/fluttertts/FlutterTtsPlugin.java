4cbe630fe77a5ad8a4809b319799ac2f4b092e6c
rishab2113 <rishab@bu.edu>
2019-04-09 04:47:49 -0400

    Initial commit

---
+++ android/src/main/java/com/tundralabs/fluttertts/FlutterTtsPlugin.java
@@ -0,0 +1,218 @@
+package com.tundralabs.fluttertts;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.speech.tts.Voice;
+import android.util.Log;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.UUID;
+
+/** FlutterTtsPlugin */
+public class FlutterTtsPlugin implements MethodCallHandler {
+  private final MethodChannel channel;
+  private TextToSpeech tts;
+  private final String tag = "TTS";
+  private final String googleTtsEngine = "com.google.android.tts";
+  String uuid;
+  Bundle bundle;
+
+  /** Plugin registration. */
+  private FlutterTtsPlugin(Context context, MethodChannel channel) {
+    this.channel = channel;
+    this.channel.setMethodCallHandler(this);
+
+    bundle = new Bundle();
+    tts = new TextToSpeech(context.getApplicationContext(), onInitListener, googleTtsEngine);
+  };
+
+  private UtteranceProgressListener utteranceProgressListener =
+      new UtteranceProgressListener() {
+        @Override
+        public void onStart(String utteranceId) {
+          channel.invokeMethod("speak.onStart", true);
+        }
+
+        @Override
+        public void onDone(String utteranceId) {
+          channel.invokeMethod("speak.onComplete", true);
+        }
+
+        @Override
+        @Deprecated
+        public void onError(String utteranceId) {
+          channel.invokeMethod("speak.onError", "Error from TextToSpeech");
+        }
+
+        @Override
+        public void onError(String utteranceId, int errorCode) {
+          channel.invokeMethod("speak.onError", "Error from TextToSpeech - " + errorCode);
+        }
+      };
+
+  private TextToSpeech.OnInitListener onInitListener =
+      new TextToSpeech.OnInitListener() {
+        @Override
+        public void onInit(int status) {
+          if (status == TextToSpeech.SUCCESS) {
+            tts.setOnUtteranceProgressListener(utteranceProgressListener);
+            channel.invokeMethod("tts.init", true);
+
+            try {
+              Locale locale = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                      ? tts.getDefaultVoice().getLocale()
+                      : tts.getDefaultLanguage();
+              if (isLanguageAvailable(locale)) {
+                tts.setLanguage(locale);
+              }
+            } catch (NullPointerException | IllegalArgumentException e){
+              Log.d(tag, "getDefaultLocale: " + e.getMessage());
+            }
+          } else {
+            Log.d(tag, "Failed to initialize TextToSpeech");
+          }
+        }
+      };
+
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_tts");
+    channel.setMethodCallHandler(new FlutterTtsPlugin(registrar.activeContext(), channel));
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("speak")) {
+      String text = call.arguments.toString();
+      speak(text);
+      result.success(1);
+    } else if (call.method.equals("stop")) {
+      stop();
+      result.success(1);
+    } else if (call.method.equals("setSpeechRate")) {
+      String rate = call.arguments.toString();
+      setSpeechRate(Float.parseFloat(rate));
+      result.success(1);
+    } else if (call.method.equals("setVolume")) {
+      String volume = call.arguments.toString();
+      setVolume(Float.parseFloat(volume), result);
+    } else if (call.method.equals("setPitch")) {
+      String pitch = call.arguments.toString();
+      setPitch(Float.parseFloat(pitch), result);
+    } else if (call.method.equals("setLanguage")) {
+      String language = call.arguments.toString();
+      setLanguage(language, result);
+    } else if (call.method.equals("getLanguages")) {
+      getLanguages(result);
+    } else if (call.method.equals("getVoices")) {
+      getVoices(result);
+    } else if (call.method.equals("setVoice")) {
+      String voice = call.arguments.toString();
+      setVoice(voice, result);
+    } else if (call.method.equals("isLanguageAvailable")) {
+      String language = ((HashMap) call.arguments()).get("language").toString();
+      Locale locale = Locale.forLanguageTag(language);
+      result.success(isLanguageAvailable(locale));
+    } else {
+      result.notImplemented();
+    }
+  }
+
+  void setSpeechRate(float rate) {
+    tts.setSpeechRate(rate);
+  }
+
+  Boolean isLanguageAvailable(Locale locale) {
+      return tts.isLanguageAvailable(locale) > 0;
+  }
+
+  void setLanguage(String language, Result result) {
+    Locale locale = Locale.forLanguageTag(language);
+    if (isLanguageAvailable(locale)) {
+      tts.setLanguage(locale);
+      result.success(1);
+    } else {
+      result.success(0);
+    }
+  }
+
+  void setVoice(String voice, Result result) {
+    for (Voice ttsVoice : tts.getVoices()) {
+      if (ttsVoice.getName().equals(voice)) {
+        tts.setVoice(ttsVoice);
+        result.success(1);
+        return;
+      }
+    }
+    Log.d(tag, "Voice name not found: " + voice);
+    result.success(0);
+  }
+
+  void setVolume(float volume, Result result) {
+    if (volume >= 0.0F && volume <= 1.0F) {
+      bundle.putFloat(TextToSpeech.Engine.KEY_PARAM_VOLUME, volume);
+      result.success(1);
+    } else {
+      Log.d(tag, "Invalid volume " + volume + " value - Range is from 0.0 to 1.0");
+      result.success(0);
+    }
+  }
+
+  void setPitch(float pitch, Result result) {
+    if (pitch >= 0.5F && pitch <= 2.0F) {
+      tts.setPitch(pitch);
+      result.success(1);
+    } else {
+      Log.d(tag, "Invalid pitch " + pitch + " value - Range is from 0.5 to 2.0");
+      result.success(0);
+    }
+  }
+ 
+  void getVoices(Result result) {
+    ArrayList<String> voices = new ArrayList<>();
+    try {
+      for (Voice voice : tts.getVoices()) {
+        voices.add(voice.getName());
+      }
+      result.success(voices);
+    } catch(NullPointerException e) {
+      Log.d(tag, "getVoices: " + e.getMessage());
+      result.success(null);
+    }
+  }
+
+  void getLanguages(Result result) {
+    ArrayList<String> locales = new ArrayList<>();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      // While this method was introduced in API level 21, it seems that it
+      // has not been implemented in the speech service side until API Level 23.
+      for (Locale locale : tts.getAvailableLanguages()) {
+        locales.add(locale.toLanguageTag());
+      }
+    } else {
+      for (Locale locale : Locale.getAvailableLocales()) {
+        if (locale.getVariant().isEmpty() && isLanguageAvailable(locale)) {
+          locales.add(locale.toLanguageTag());
+        }
+      }
+    }
+    result.success(locales);
+  }
+
+  void speak(String text) {
+    uuid = UUID.randomUUID().toString();
+    tts.speak(text, TextToSpeech.QUEUE_FLUSH, bundle, uuid);
+  }
+
+  void stop() {
+    tts.stop();
+  }
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,207 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+enum TtsState { playing, stopped }
+
+class _MyAppState extends State<MyApp> {
+  FlutterTts flutterTts;
+  dynamic languages;
+  dynamic voices;
+  String language;
+  String voice;
+
+  String _newVoiceText;
+
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+
+  @override
+  initState() {
+    super.initState();
+    initTts();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    if (Platform.isAndroid) {
+      flutterTts.ttsInitHandler(() {
+        _getLanguages();
+        _getVoices();
+      });
+    } else if (Platform.isIOS) {
+      _getLanguages();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getLanguages() async {
+    languages = await flutterTts.getLanguages;
+    if (languages != null) setState(() => languages);
+  }
+
+  Future _getVoices() async {
+    voices = await flutterTts.getVoices;
+    if (voices != null) setState(() => voices);
+  }
+
+  Future _speak() async {
+    if (_newVoiceText != null) {
+      if (_newVoiceText.isNotEmpty) {
+        var result = await flutterTts.speak(_newVoiceText);
+        if (result == 1) setState(() => ttsState = TtsState.playing);
+      }
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  List<DropdownMenuItem<String>> getLanguageDropDownMenuItems() {
+    var items = List<DropdownMenuItem<String>>();
+    for (String type in languages) {
+      items.add(DropdownMenuItem(value: type, child: Text(type)));
+    }
+    return items;
+  }
+
+  List<DropdownMenuItem<String>> getVoiceDropDownMenuItems() {
+    var items = List<DropdownMenuItem<String>>();
+    for (String type in voices) {
+      items.add(DropdownMenuItem(value: type, child: Text(type)));
+    }
+    return items;
+  }
+
+  void changedLanguageDropDownItem(String selectedType) {
+    setState(() {
+      language = selectedType;
+      flutterTts.setLanguage(language);
+    });
+  }
+
+  void changedVoiceDropDownItem(String selectedType) {
+    setState(() {
+      voice = selectedType;
+      flutterTts.setVoice(voice);
+    });
+  }
+
+  void _onChange(String text) {
+    setState(() {
+      _newVoiceText = text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Flutter TTS'),
+            ),
+            body: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(children: [
+                  inputSection(),
+                  btnSection(),
+                  languages != null ? languageDropDownSection() : Text(""),
+                  voices != null ? voiceDropDownSection() : Text("")
+                ]))));
+  }
+
+  Widget inputSection() => Container(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
+      child: TextField(
+        onChanged: (String value) {
+          _onChange(value);
+        },
+      ));
+
+  Widget btnSection() => Container(
+      padding: EdgeInsets.only(top: 50.0),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        _buildButtonColumn(
+            Colors.green, Colors.greenAccent, Icons.play_arrow, 'PLAY', _speak),
+        _buildButtonColumn(
+            Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop)
+      ]));
+
+  Widget languageDropDownSection() => Container(
+      padding: EdgeInsets.only(top: 50.0),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        DropdownButton(
+          value: language,
+          items: getLanguageDropDownMenuItems(),
+          onChanged: changedLanguageDropDownItem,
+        )
+      ]));
+
+  Widget voiceDropDownSection() => Container(
+      padding: EdgeInsets.only(top: 50.0),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        DropdownButton(
+          value: voice,
+          items: getVoiceDropDownMenuItems(),
+          onChanged: changedVoiceDropDownItem,
+        )
+      ]));
+
+  Column _buildButtonColumn(Color color, Color splashColor, IconData icon,
+      String label, Function func) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+              icon: Icon(icon),
+              color: color,
+              splashColor: splashColor,
+              onPressed: () => func()),
+          Container(
+              margin: const EdgeInsets.only(top: 8.0),
+              child: Text(label,
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w400,
+                      color: color)))
+        ]);
+  }
+}
